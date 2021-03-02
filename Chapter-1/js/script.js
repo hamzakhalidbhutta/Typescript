0a56a5d20e4dfb939ba0d2b559bac60e808c02de
Hamza Khalid Bhutta <hamzakhalidbhutta430@gmail.com>
2021-03-02 22:53:55 +0500

    Updated Chapter-1_Dictionaries

--- Chapter-1/js/script.js
+++ Chapter-1/js/script.js
@@ -1,17 +1,17 @@
 var booksStore = {};
-booksStore['DS'] = {
+booksStore["DS"] = {
     name: "Data Science",
     price: 2000,
     isAvailable: true
 };
-booksStore['ML'] = {
+booksStore["ML"] = {
     name: "Python for Datascience",
     price: 2500,
     isAvailable: true
 };
-console.log(booksStore['ML']);
-var booksShelf;
-booksShelf['name'] = "AI";
-booksShelf['price'] = 5000;
-booksShelf['isAvailable'] = false;
+console.log(booksStore["ML"]);
+var booksShelf = {};
+booksShelf["name"] = "AI";
+booksShelf["price"] = 5000;
+booksShelf["isAvailable"] = false;
 console.log(booksShelf);
