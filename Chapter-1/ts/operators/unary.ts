3eb8c3cb9c26bbd29741467073d22b153423267f
Hamza Khalid Bhutta <hamzakhalidbhutta430@gmail.com>
2021-03-03 17:15:41 +0500

    Chapter-1_Uniary

--- Chapter-1/ts/operators/unary.ts
+++ Chapter-1/ts/operators/unary.ts
@@ -2,10 +2,11 @@
     *   When the plus operator is used with only a single argument, it acts as a shorthand conversion to a number.
     *   The unary minus operator also converts the type to number and changes its sign.
 */
-var str = '5';
+
+const str: string = '5';
 // 5: number
-var convertToNum = +str;
-console.log(convertToNum);
+const convertToNum = +str;
+console.log(convertToNum)
 // -5: number
-var negative = -str;
-console.log(negative);
+const negative = -str;
+console.log(negative)
