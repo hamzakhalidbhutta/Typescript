3ed601d705f083f1d6a6e3b77918f22b6953aa23
Hamza Khalid Bhutta <hamzakhalidbhutta430@gmail.com>
2021-03-03 17:16:35 +0500

    Update binary.ts

--- Chapter-1/ts/operators/binary.ts
+++ Chapter-1/ts/operators/binary.ts
@@ -24,15 +24,5 @@ console.log(num)
 
 // '51': string
 const str = 5 + '1';
+console.log(str)
 
-/*
-    *   When the plus operator is used with only a single argument, it acts as a shorthand conversion to a number.
-    *   The unary minus operator also converts the type to number and changes its sign.
-*/
-
-// 5: number
-const convertToNum = +str;
-console.log(convertToNum)
-// -5: number
-const negative = -str;
-console.log(negative)
